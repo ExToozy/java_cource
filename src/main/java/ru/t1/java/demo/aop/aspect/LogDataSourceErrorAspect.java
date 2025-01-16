37a0f8138d009c1dfd247ec421b4ca5f40f33de8
ExToozy <ilya.makarov.04@mail.ru>
2025-01-16 14:50:23 +0700

    add aspect

---
+++ src/main/java/ru/t1/java/demo/aop/aspect/LogDataSourceErrorAspect.java
@@ -0,0 +1,36 @@
+package ru.t1.java.demo.aop.aspect;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import ru.t1.java.demo.model.DataSourceErrorLog;
+import ru.t1.java.demo.repository.ErrorLogRepository;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class LogDataSourceErrorAspect {
+
+    private final ErrorLogRepository errorLogRepository;
+
+    @AfterThrowing(value = "@annotation(ru.t1.java.demo.aop.annotation.LogDataSourceError)", throwing = "e")
+    public void LogDataSourceErrorAfterThrowing(JoinPoint joinPoint, Exception e) {
+        log.info("Сохраняем сообщение об ошибке");
+        errorLogRepository.save(
+                DataSourceErrorLog.builder()
+                        .methodSignature(joinPoint.getSignature().toLongString())
+                        .stackTrace(getLastTraceElement(e))
+                        .errorMessage(e.getMessage())
+                        .build()
+        );
+
+    }
+
+    private String getLastTraceElement(Exception e) {
+        return e.getStackTrace()[0].toString();
+    }
+}
