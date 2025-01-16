a49489fa99c5561689cd285a63f2b4af16829918
ExToozy <ilya.makarov.04@mail.ru>
2025-01-16 14:49:17 +0700

    add enum validation

---
+++ src/main/java/ru/t1/java/demo/validation/annotation/ValueOfEnum.java
@@ -0,0 +1,26 @@
+package ru.t1.java.demo.validation.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import ru.t1.java.demo.validation.validator.ValueOfEnumValidator;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ElementType.FIELD})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = ValueOfEnumValidator.class)
+public @interface ValueOfEnum {
+    Class<? extends Enum<?>> enumClass();
+
+    String message() default "must be any of enum {enumClass}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
