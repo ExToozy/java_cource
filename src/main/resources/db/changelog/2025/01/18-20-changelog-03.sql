d7270353135ce39a3136176fd18063e4e7c6c422
ExToozy <ilya.makarov.04@mail.ru>
2025-01-16 12:17:39 +0700

    fix liquibase migration

--- src/main/resources/db/changelog/2025/01/18-20-changelog-03.sql
+++ src/main/resources/db/changelog/2025/01/18-20-changelog-03.sql
@@ -1,11 +1,9 @@
 create table transaction
 (
-    id             bigint generated always as identity primary key,
-    amount         numeric,
-    completed_at   timestamp without time zone,
-    client_from_id bigint,
-    client_to_id   bigint,
-    foreign key (client_to_id) references account (id),
-    foreign key (client_from_id) references account (id)
+    id           bigint generated always as identity primary key,
+    amount       numeric,
+    completed_at timestamp without time zone,
+    client_id    bigint,
+    foreign key (client_id) references account (id)
 );
 
