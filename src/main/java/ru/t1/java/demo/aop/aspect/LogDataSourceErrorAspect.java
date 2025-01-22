5947348e5b07a2aea47715f46d18115c2e4c220f
ExToozy <ilya.makarov.04@mail.ru>
2025-01-22 22:18:59 +0700

    create metric aspect and update log error aspect

--- src/main/java/ru/t1/java/demo/aop/aspect/LogDataSourceErrorAspect.java
+++ src/main/java/ru/t1/java/demo/aop/aspect/LogDataSourceErrorAspect.java
@@ -1,11 +1,13 @@
 package ru.t1.java.demo.aop.aspect;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import ru.t1.java.demo.kafka.producer.KafkaErrorLogProducer;
 import ru.t1.java.demo.model.DataSourceErrorLog;
 import ru.t1.java.demo.repository.ErrorLogRepository;
 
@@ -20,21 +22,30 @@ public class LogDataSourceErrorAspect {
 
     private final ErrorLogRepository errorLogRepository;
 
+    private final KafkaErrorLogProducer errorLogProducer;
+
     @AfterThrowing(
             value = "@annotation(ru.t1.java.demo.aop.annotation.LogDataSourceError)",
             throwing = "e"
     )
-    public void logDataSourceErrorAfterThrowing(JoinPoint joinPoint, Exception e) {
+    public void logDataSourceErrorAfterThrowing(JoinPoint joinPoint, Exception e) throws JsonProcessingException {
         log.info("Сохраняем сообщение об ошибке");
-        errorLogRepository.save(
-                DataSourceErrorLog.builder()
-                        .methodSignature(joinPoint.getSignature().toLongString())
-                        .stackTrace(toStackTraceString(e))
-                        .errorMessage(e.getMessage())
-                        .build()
-        );
 
+        DataSourceErrorLog errorLog = DataSourceErrorLog.builder()
+                .methodSignature(joinPoint.getSignature().toLongString())
+                .stackTrace(toStackTraceString(e))
+                .errorMessage(e.getMessage())
+                .build();
+
+        try {
+            errorLogProducer.send(errorLog);
+        } catch (Exception exception) {
+            log.error("Error occurred while trying send error log message to kafka: ", e);
+            log.info("Trying save error log to db");
+            errorLogRepository.save(errorLog);
+        }
     }
+
 
     private String toStackTraceString(Exception e) {
         StringWriter sw = new StringWriter();
---
+++ src/main/java/ru/t1/java/demo/aop/aspect/MetricAspect.java
@@ -0,0 +1,71 @@
+package ru.t1.java.demo.aop.aspect;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import ru.t1.java.demo.aop.annotation.Metric;
+import ru.t1.java.demo.dto.metric.MetricDto;
+import ru.t1.java.demo.kafka.producer.KafkaMetricProducer;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class MetricAspect {
+
+    private final KafkaMetricProducer metricProducer;
+
+    private final ObjectMapper objectMapper;
+
+    @Around("@annotation(ru.t1.java.demo.aop.annotation.Metric)")
+    public Object sendMetricIfTimeExceed(ProceedingJoinPoint pJoinPoint) throws Throwable {
+
+        long beforeTime = System.currentTimeMillis();
+        Object result = pJoinPoint.proceed();
+        long afterTime = System.currentTimeMillis();
+
+        Annotation metricAnnotation = getMetricAnnotation(pJoinPoint);
+
+        if (metricAnnotation != null) {
+            try {
+                long executionTime = afterTime - beforeTime;
+                long limit = ((Metric) metricAnnotation).executionTimeMillisLimit();
+                if (executionTime > limit) {
+                    sendMetricToKafka(pJoinPoint, executionTime);
+                }
+            } catch (Throwable e) {
+                log.error("Error occurred while trying send metric message to kafka: ", e);
+            }
+        }
+
+        return result;
+    }
+
+    private Annotation getMetricAnnotation(ProceedingJoinPoint pJoinPoint) {
+        return Arrays.stream(((MethodSignature) pJoinPoint.getSignature())
+                        .getMethod()
+                        .getAnnotations())
+                .filter(annotation -> annotation instanceof Metric)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private void sendMetricToKafka(ProceedingJoinPoint pJoinPoint, long executionTime) throws JsonProcessingException {
+        MetricDto metric = MetricDto.builder()
+                .executionTime(executionTime)
+                .methodName(pJoinPoint.toLongString())
+                .jsonArgs(objectMapper.writeValueAsString(pJoinPoint.getArgs()))
+                .build();
+
+        metricProducer.send(metric);
+    }
+}
